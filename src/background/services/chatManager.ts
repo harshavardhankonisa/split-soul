8b314f5b0f8dedbeb9d4bd1a7d961f4c6da6e3b9
harshavardhankonisa <harshavardhankonisa@gmial.com>
2025-10-19 11:35:46 +0530

    trigger for multi soul opinions

--- src/background/services/chatManager.ts
+++ src/background/services/chatManager.ts
@@ -1,9 +1,23 @@
 import type { Chat } from '../../interface/database'
 import { createChat, getAllChats } from '../../services/dexie/collections/chat'
+import { getAllUsers } from '../../services/dexie/collections/user'
 
 export class ChatManager {
-  public addChat(chat: Omit<Chat, 'id' | 'createdAt' | 'vector'>): void {
-    createChat(chat)
+  private async triggerMultiSoulOpinions(message: string) {
+    const customSouls = (await getAllUsers()).filter(
+      u => u.username !== 'Main Soul' && u.username !== 'Main Body' && u.isActive
+    )
+    for (const soul of customSouls) {
+      console.log(soul, message)
+    }
+  }
+
+  public async addChat(chat: Omit<Chat, 'id' | 'createdAt' | 'vector'>) {
+    await createChat(chat)
+
+    if (chat.user === 'Main Soul') {
+      await this.triggerMultiSoulOpinions(chat.message)
+    }
   }
 
   public async getActiveChats(): Promise<Chat[]> {
